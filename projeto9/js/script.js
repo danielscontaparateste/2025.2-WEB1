70f06377d85c81aba1b05ec66d549b40f1b0f5cd
Daniel Ferreira <danielse@ifce.edu.br>
2025-10-29 17:53:17 -0300

    Códigos de aula

---
+++ projeto9/js/script.js
@@ -0,0 +1,13 @@
+function imprimirMensagem(){
+    alert("Olá Mundo");
+}
+
+function backgroundRed(){
+    var bd = document.querySelector("body");
+    bd.style.backgroundColor = "red";
+}
+
+function backgroundWhite(){
+    var bd = document.querySelector("body");
+    bd.style.backgroundColor = "white";
+}
